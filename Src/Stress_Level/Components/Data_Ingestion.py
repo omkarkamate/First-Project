55a21403fc55e2b116a43820c0ca5afa61bdd7e2
omkarkamate <omkarkamate2004@gmail.com>
2026-02-19 20:11:58 +0530

    Data Ingestion

--- Src/Stress_Level/Components/Data_Ingestion.py
+++ Src/Stress_Level/Components/Data_Ingestion.py
@@ -4,19 +4,28 @@ from Src.Stress_Level.Exception import Cu_Exception
 import sys
 from Src.Stress_Level.Logging import logging
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
 def Get_Data():
     logging.info("Reading dataset from File Explorer")
     df = pd.read_csv("E:\dataset\Smartphone_Usage_Productivity_Dataset_50000.csv")
     df["Stress_Level"]=(
-        df["Social_Media_Hours"]*0.2 -
-        df["Sleep_Hours"]*0.4 +
-        df["Weekend_Screen_Time_Hours"]*0.1 +
+        df["Social_Media_Hours"]*0.4 -
+        df["Sleep_Hours"]*0.7 +
+        df["Weekend_Screen_Time_Hours"]*0.2 -
         df["Age"]*0.2 -
-        df["Caffeine_Intake_Cups"]*0.2
+        df["Caffeine_Intake_Cups"]*0.2 +
+        df["Daily_Phone_Hours"]*0.3 -
+        df["Work_Productivity_Score"]*0.3
     )
+
+    MMScaler=MinMaxScaler()
+    df["Stress_Level"]=MMScaler.fit_transform(df[["Stress_Level"]])*10
+
     df["Stress_Level"]=np.round(df["Stress_Level"],3)
+
+
     path=os.path.join("artifacts","raw.csv")
     os.makedirs("artifacts",exist_ok=True)
     df.to_csv(path,index=False)

b351247bb028690aa18db21333360e68cd0e251f
omkarkamate <omkarkamate2004@gmail.com>
2026-02-19 19:34:37 +0530

    Data Ingestion

--- Src/Stress_Level/Components/Data_Ingestion.py
+++ Src/Stress_Level/Components/Data_Ingestion.py
@@ -3,11 +3,20 @@ import pandas as pd
 from Src.Stress_Level.Exception import Cu_Exception
 import sys
 from Src.Stress_Level.Logging import logging
+import numpy as np
 
 
 def Get_Data():
     logging.info("Reading dataset from File Explorer")
     df = pd.read_csv("E:\dataset\Smartphone_Usage_Productivity_Dataset_50000.csv")
+    df["Stress_Level"]=(
+        df["Social_Media_Hours"]*0.2 -
+        df["Sleep_Hours"]*0.4 +
+        df["Weekend_Screen_Time_Hours"]*0.1 +
+        df["Age"]*0.2 -
+        df["Caffeine_Intake_Cups"]*0.2
+    )
+    df["Stress_Level"]=np.round(df["Stress_Level"],3)
     path=os.path.join("artifacts","raw.csv")
     os.makedirs("artifacts",exist_ok=True)
     df.to_csv(path,index=False)

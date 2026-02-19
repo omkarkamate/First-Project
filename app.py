aab0cab408d6100d05da5e657e513fe5e9ddf837
omkarkamate <omkarkamate2004@gmail.com>
2026-02-19 11:57:37 +0530

    change

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from Src.Stress_Level.Logging import logging
 from Src.Stress_Level.Exception import Cu_Exception
 import sys
+from Src.Stress_Level.Components.Data_Ingestion import Get_Data
 
 if __name__=="__main__":
     try:
-        a=1/0
+        obj=Get_Data()
+        print("Featurs :- ")
+        print(obj.columns)
+        print("\nFirst five Rows : \n")
+        print(obj)
 
     except(Exception) as e:
         raise Cu_Exception(e,sys)

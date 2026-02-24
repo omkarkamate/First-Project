ef6a707eeaf8fd2eee979180491a76893614108b
omkarkamate <omkarkamate2004@gmail.com>
2026-02-24 13:20:06 +0530

    changes

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from Src.Stress_Level.Logging import logging
 from Src.Stress_Level.Exception import Cu_Exception
 import sys
-from Src.Stress_Level.Components.Data_Ingestion import Get_Data
+from Src.Stress_Level.Components.Data_Preprocessing import Data_preprocessing
+import os
 
 if __name__=="__main__":
     try:
-        obj=Get_Data()
-        print("Featurs :- ")
-        print(obj.columns)
-        print("\nFirst five Rows : \n")
-        print(obj) 
+
+        obj=Data_preprocessing()
+        aa=obj.Data_Preprocess(os.path.join("artifacts","train_data"),os.path.join("artifacts","test_data"))
+        print(aa)
+        
 
     except(Exception) as e:
         raise Cu_Exception(e,sys)

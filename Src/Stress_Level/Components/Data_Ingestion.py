f90534f45979237bf4b72ccf07d326878ffb3f5b
omkarkamate <omkarkamate2004@gmail.com>
2026-02-24 13:15:15 +0530

    read data and split into train and test

--- Src/Stress_Level/Components/Data_Ingestion.py
+++ Src/Stress_Level/Components/Data_Ingestion.py
@@ -5,7 +5,7 @@ import sys
 from Src.Stress_Level.Logging import logging
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.model_selection import train_test_split
 
 def Get_Data():
     logging.info("Reading dataset from File Explorer")
@@ -25,11 +25,20 @@ def Get_Data():
 
     df["Stress_Level"]=np.round(df["Stress_Level"],3)
 
+    df.drop(columns=["User_ID"], inplace=True)
 
     path=os.path.join("artifacts","raw.csv")
     os.makedirs("artifacts",exist_ok=True)
     df.to_csv(path,index=False)
     logging.info("Insert dataset to new file (artifacts/raw.csv)")
+
+    Train,Test=train_test_split(df,test_size=0.2,random_state=42)
+    Train_C=os.path.join("artifacts","train_data")
+    Test_C=os.path.join("artifacts","test_data")
+    Train.to_csv(Train_C,index=False)
+    Test.to_csv(Test_C,index=False)
+
+
     return(df.head())
 
 
